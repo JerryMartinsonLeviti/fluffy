a74bbb6cfe76873de39a2f034c11dd9441262451
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-17 06:11:42 +0000

    Updating to latest FlutterFlow output.

--- lib/liaison_flow/liaison_dashboard/liaison_dashboard_widget.dart
+++ lib/liaison_flow/liaison_dashboard/liaison_dashboard_widget.dart
@@ -1,6 +1,7 @@
 import '/components/icon_manage_events/icon_manage_events_widget.dart';
 import '/components/icon_requestto_book_plane/icon_requestto_book_plane_widget.dart';
 import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/home_page/dev_mode/dev_p_k_selection/dev_p_k_selection_widget.dart';
@@ -87,6 +88,20 @@ class _LiaisonDashboardWidgetState extends State<LiaisonDashboardWidget>
         appBar: AppBar(
           backgroundColor: FlutterFlowTheme.of(context).primary,
           automaticallyImplyLeading: false,
+          leading: FlutterFlowIconButton(
+            borderColor: Colors.transparent,
+            borderRadius: 30.0,
+            borderWidth: 1.0,
+            buttonSize: 60.0,
+            icon: const Icon(
+              Icons.arrow_back_rounded,
+              color: Colors.white,
+              size: 30.0,
+            ),
+            onPressed: () async {
+              context.pop();
+            },
+          ),
           title: Text(
             'Page Title',
             style: FlutterFlowTheme.of(context).headlineMedium.override(

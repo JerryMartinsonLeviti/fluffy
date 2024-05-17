7b298bab7f55c046009fdd719b8dddfe06d7efaf
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-17 23:06:23 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/custom_functions.dart
+++ lib/flutter_flow/custom_functions.dart
@@ -76,9 +76,9 @@ String? filterACV(
   String typeKey,
   List<AcvRow> acvIn,
 ) {
-  // Filter the list for rows where typeKey is not null
+  // Filter the list for rows where the typeKey column is not null
   List<AcvRow> filteredList =
-      acvIn.where((row) => row.typeKey != null).toList();
+      acvIn.where((row) => row.data[typeKey] != null).toList();
 
   // Return the typeKey if any rows match, otherwise return null
   if (filteredList.isNotEmpty) {

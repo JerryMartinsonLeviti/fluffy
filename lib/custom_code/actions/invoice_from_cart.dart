62f1f5b08ceab5188912964aad71c3fac79e9fff
Jerry Martinson <75230149+JoonProCTO@users.noreply.github.com>
2024-05-25 14:15:34 -0700

    update

--- lib/custom_code/actions/invoice_from_cart.dart
+++ lib/custom_code/actions/invoice_from_cart.dart
@@ -16,5 +16,27 @@ Future<CartInvoiceStruct> invoiceFromCart(CartsRow cart) async {
   // just create dummy return
   CartInvoiceStruct dummyInvoice = CartInvoiceStruct();
 
+  // now lets' create a dummy invoice by filing up the dummyInvoice object fields defined in the project
+  dummyInvoice.packages = PackageSummaryStruct();  // now look at the struct definition in the project for PackageSummaryStruct
+
+  PackageSummaryStruct package_summary = PackageSummaryStruct();
+  List<PackageLineStruct> package_lines = [];
+  package_lines.add(PackageLineStruct());
+  package_lines[0].packageName = "Dummy Package Line";
+  package_summary.packageLines = package_lines;
+
+  // FnbItemLineStruct fnb_item_line = FnbItemLineStruct();
+  // fnb_item_line.itemName = "Dummy Fnb Item";
+  // FoodAndBevSummaryStruct fnb_summary = FoodAndBevSummaryStruct();
+  // fnb_summary.fnbItemLines = [fnb_item_line];
+  // dummyInvoice.fnbMinimums = fnb_summary;
+
+
+
+
+  // package_summary.packageName = "Dummy Package";
+  dummyInvoice.packages = package_summary;
+  // fill up the dummy invoice with some dummy data
+
   return dummyInvoice;
 }
